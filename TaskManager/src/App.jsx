208df81303bc17b6df27f3368099b45297d67b40
ADI-7065 <Adityamay0101@gmail.com>
2026-04-19 16:46:53 +0530

    make basic structure of task manager app

--- TaskManager/src/App.jsx
+++ TaskManager/src/App.jsx
@@ -1,40 +1,49 @@
 import "./App.css";
-
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faTrashCan,
-//   faClipboardCheck,
-// } from "@fortawesome/free-solid-svg-icons";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import {
+  faTrashCan,
+  faClipboardCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {};
+  const addTask = () => {
+    var a = document.getElementById("i").value;
+    setTasks([...tasks, a]);
+  };
+  const deleteTask = (t) => {
+    setTasks(tasks.filter((a) => a != t));
+  };
 
   return (
     <div className="bg-light p-5 rounded">
       <h1 className="mb-4 text-center">
-        Task Manager
+        Task Manager <FontAwesomeIcon icon={faClipboardCheck} />
       </h1>
       <div className="d-flex mb-4">
-        <input
-          className="form-control"
-          placeholder="Enter task..."
-        />
-        <button className="bg-primary text-white py-1 px-4 ms-2 rounded">
+        <input id="i" className="form-control" placeholder="Enter task..." />
+        <button
+          onClick={() => addTask()}
+          className="btn btn-primary py-1 px-4 ms-2 rounded"
+        >
           Add
         </button>
       </div>
 
-      <ul className="">
-        <li>
-          <span></span>
-          <button>
-          </button>
-        </li>
+      <ul>
+        {tasks.map((t) => (
+          <li className="my-2">
+            <span>{t}</span>
+            <button
+              onClick={() => deleteTask(t)}
+              className="btn btn-danger ms-3"
+            >
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
